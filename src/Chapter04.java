76f29f49bb91ae729a3bcce009465870564d309a
yossu-y <yangyuub@gmail.com>
2022-07-21 00:24:03 +0900

    first commit

---
+++ src/Chapter04.java
@@ -0,0 +1,11 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String str;
+		str = "八木陽祐";
+		System.out.println(str);
+		str = "水";
+		System.out.println(str);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
