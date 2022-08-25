a29f23b69447cb0b71040097e5d75f2f0ce904b7
yossu-y <yangyuub@gmail.com>
2022-08-25 14:20:31 +0900

    update

---
+++ src/北ソフト工房/lesson1.java
@@ -0,0 +1,41 @@
+package 北ソフト工房;
+
+public class lesson1 {
+	public static void main(String[] args) {
+		System.out.print("Hello World");
+		System.out.println("Hello World");
+		System.out.printf("Hello World");
+		
+		System.out.println("#############");
+		System.out.println("#############");
+		
+//		int x = 11;
+//		System.out.println(x);
+		
+		int x = 13;
+		int y = 17;
+		
+		System.out.println("x =" + x + ", y =" + y);
+		
+		x = x + 17;
+		System.out.println(x);
+		
+		System.out.println(13 * 17);
+		
+		int z = 7;
+		z *= 3;
+		System.out.println(x);
+		z /= 2;
+		System.out.println(z);
+		
+		int b = 3;
+		
+		System.out.println(b * 2);
+		System.out.println(b * 3);
+		System.out.println(b * 4);
+				
+		
+		
+		
+    }
+}
---
+++ src/北ソフト工房/lesson2.java
@@ -0,0 +1,31 @@
+package 北ソフト工房;
+
+public class lesson2 {
+	public static void main( String[] args )  {
+		
+	    String name = "Bob";
+	    System.out.println(name);
+	    
+	    int x = 15;
+	    System.out.println(x);
+	    
+	    int y = 5;
+	    System.out.println(y);
+	    System.out.println(y * y);
+	    System.out.println(y * y * y);
+	    
+	    int a = 2;
+	    int b = 3;
+	    System.out.println(a + b);
+	    System.out.println(a - b);
+	    System.out.println(a * b);
+	    System.out.println(a / b);
+	    
+	    System.out.println((a + b) / 2);
+	    
+	    int age = 26;
+	    System.out.println(age * 265);
+	    
+	    
+	}
+}
