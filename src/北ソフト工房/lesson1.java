383921c3d60c6c44255700bc4d4726462ddc2eac
yossu-y <yangyuub@gmail.com>
2022-08-26 22:47:42 +0900

    update

--- src/北ソフト工房/lesson1.java
+++ src/北ソフト工房/lesson1.java
@@ -2,40 +2,71 @@ package 北ソフト工房;
 
 public class lesson1 {
 	public static void main(String[] args) {
-		System.out.print("Hello World");
+		System.out.println("練習問題1-1");
 		System.out.println("Hello World");
-		System.out.printf("Hello World");
 		
-		System.out.println("#############");
-		System.out.println("#############");
+		System.out.println("練習問題1-2");
+		int a = 11;
+		System.out.println("a=" + a);
 		
-//		int x = 11;
-//		System.out.println(x);
+		System.out.println("練習問題1-3");
+		int b = 13;
+		int c = 17;
+		System.out.println("b=" + b);
+		System.out.println("c=" + c);
 		
-		int x = 13;
-		int y = 17;
+		System.out.println("練習問題1-4");
+		int d = b + c;
+		System.out.println("d=" + d);
 		
-		System.out.println("x =" + x + ", y =" + y);
+		System.out.println("練習問題1-5");
+		System.out.println(b * c);
 		
-		x = x + 17;
-		System.out.println(x);
+		System.out.println("練習問題1-6");
+		int e = 7;
+		int f = e * 3;
+		System.out.println(f);
+		int g = f / 2;
+		System.out.println(g);
 		
-		System.out.println(13 * 17);
+		System.out.println("練習問題1-7");
+		int h = 3;
+		int i = 7;
 		
-		int z = 7;
-		z *= 3;
-		System.out.println(x);
-		z /= 2;
-		System.out.println(z);
+		int j = h;
+		h = i;
+		i = j;
+		System.out.println("h= ," + h + "i=" + i);
 		
-		int b = 3;
+		System.out.println("練習問題1-8");
+		int k = 19;
+		int l = 23;
+		int m = k * l;
+		System.out.println(m);
 		
-		System.out.println(b * 2);
-		System.out.println(b * 3);
-		System.out.println(b * 4);
-				
+		System.out.println("練習問題1-9");
+		int n = 4;
+		System.out.println(n * 2);
+		System.out.println(n * 3);
+		System.out.println(n * 4);
+		
+		System.out.println("練習問題1-10");
+		int o = 3;
+		System.out.println(o);
+		System.out.println(o * o);
+		System.out.println(o * o * o);
+		
+		System.out.println("練習問題1-11");
+		int p = 11;
+		System.out.println("商=" + (p / 4) + "余り=" + (p % 5));
 		
 		
+		System.out.println("練習問題1-12");
+		int q = 7;
+		q++;
+		System.out.println(q);
+		q--;
+		System.out.println(q);
 		
     }
 }
