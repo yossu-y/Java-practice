6805e891807e7cff9dfffdc852e85eb3f8e1fce2
yossu-y <yangyuub@gmail.com>
2022-08-27 12:27:50 +0900

    update

---
+++ src/北ソフト工房/lesson4.java
@@ -0,0 +1,35 @@
+package 北ソフト工房;
+
+public class lesson4 {
+	public static void main(String[] args) {
+		System.out.println("練習問題4-1");
+		String word = "SPAM";
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i + "." + word);
+		}
+		
+		System.out.println("練習問題4-2");
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(3 * i);
+		}
+		
+		System.out.println("練習問題4-3");
+		int a = 1;
+		for (int b = 1; b <= 8; b++) {
+			a *= 2;
+			System.out.println("2の" + b + "乗" + a);
+		}
+		
+		System.out.println("練習問題4-4");
+		int c = 1;
+		for (int d = 2; d  <= 7; d++) {
+			c *= d; 
+			System.out.println(c);
+		}
+		
+		System.out.println("練習問題4-5");
+		System.out.println("練習問題4-6");
+		
+		
+	}
+}
