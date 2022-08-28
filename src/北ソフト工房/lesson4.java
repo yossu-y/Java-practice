bfbf3566b29e652edde671764e343c96969a65af
yossu-y <yangyuub@gmail.com>
2022-08-28 12:33:50 +0900

    update

--- src/北ソフト工房/lesson4.java
+++ src/北ソフト工房/lesson4.java
@@ -35,6 +35,22 @@ public class lesson4 {
 		
 		System.out.println("練習問題4-5");
 		System.out.println("練習問題4-6");
+		System.out.println("練習問題4-7");
+		System.out.println("練習問題4-8");
+		System.out.println("練習問題4-9");
+		System.out.println("練習問題4-10");
+		System.out.println("練習問題4-11");
+		System.out.println("練習問題4-12");
+		System.out.println("練習問題4-13");
+		System.out.println("練習問題4-14");
+		System.out.println("練習問題4-15");
+		System.out.println("練習問題4-16");
+		System.out.println("練習問題4-17");
+		System.out.println("練習問題4-18");
+		System.out.println("練習問題4-19");
+		System.out.println("練習問題4-20");
+		System.out.println("練習問題4-21");
+		System.out.println("練習問題4-22");
 		
 		
 	}
