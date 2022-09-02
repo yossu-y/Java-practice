5fcdd787477b42c4b2ddfadee6d5ebab754ee934
yossu-y <yangyuub@gmail.com>
2022-09-02 16:24:11 +0900

    update

--- src/一週間で身につくJava言語の基本/Basic_Day4.java
+++ src/一週間で身につくJava言語の基本/Basic_Day4.java
@@ -1,5 +1,114 @@
 package 一週間で身につくJava言語の基本;
 
 public class Basic_Day4 {
-
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			System.out.print(i + " ");
+		}
+		System.out.println();
+		
+		for (int i = 1; i <= 2; i++) {
+			for (int j = 1; j <= 3; j++) {
+				int k = i + j;
+				System.out.print(i + "+" + j + "=" + k + " ");
+			}
+			System.out.println();
+		}
+		
+		for (int a = 1; a <= 3; a++) {
+			for (int b = 1; b <= 3; b++) {
+				for (int c = 1; c <= 3; c++) {
+					System.out.print(a + "+" + b + "+" + c + "=" + (a + b + c) + " ");
+				}
+				System.out.println(" ");
+			}
+		}
+		
+		int d = 0;
+		while (d <= 5) {
+			System.out.print(d + " ");
+			d++;
+		}
+		System.out.println();
+		
+		int num = (int)(Math.random() * 10) + 1;
+		System.out.println(num);
+		for (int e = 0 ; e < num ; e++) {
+			System.out.print("■ ");
+		}
+		
+		System.out.println();
+		
+		int f = 0;
+		System.out.println(num);
+		while (f < num) {
+			System.out.print("■ ");
+			f++;
+		}
+		
+		System.out.println();
+		
+		int g = 0;
+		while (g <= 3) {
+			System.out.println("a = " + g);
+			g++;
+		}
+		
+		for (int h = 0; h <= 3; h++) {
+			System.out.println("h = " + h);
+		}
+		
+//		int i = 3;
+//		while (i >= 0) {
+//			System.out.println("i = " + i);
+//			i--;
+//		}
+		
+		for (int k = 3; k >= 0; k--) {
+			System.out.println(k);
+		}
+		
+		System.out.println("#############");
+		
+		while (true) {
+			int number = (int)(Math.random() * 10) + 1;
+			System.out.println(number);
+			if (number == 10) {
+				break;
+			}
+		}
+		
+		System.out.println("#############");
+		
+		while (true) {
+			int k = (int)(Math.random() * 100) + 1;
+			System.out.print(k + " ");
+			if (k % 10 == 0) {
+				break;
+			}
+		}
+		
+		System.out.println();
+		System.out.println("###############");
+		
+		int max_value = 0;
+		
+		for (int m = 1; m <= 5; m++) {
+			int l = (int)(Math.random() * 10) + 1;
+			System.out.print(l + " ");
+			if (m == 1) {
+				max_value = l;
+			} else {
+				if (max_value < l) {
+					max_value = l;
+				}
+			}
+				
+		}
+		System.out.println("最大値：" + max_value);
+		
+		
+	}
+	
+	
 }
